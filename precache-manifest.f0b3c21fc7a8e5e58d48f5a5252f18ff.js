f1a8ab087c3faf92d344654a8ed51e1ae175ad34
Roy Reyna <royreyna7@yahoo.com>
2020-06-11 18:12:48 -0500

    Updates

---
+++ precache-manifest.f0b3c21fc7a8e5e58d48f5a5252f18ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d3161379c0f44135e6c489c4cf9272e",
+    "url": "/michael-scott-memoryGame/index.html"
+  },
+  {
+    "revision": "a361b9495fa6be1253e5",
+    "url": "/michael-scott-memoryGame/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a361b9495fa6be1253e5",
+    "url": "/michael-scott-memoryGame/static/js/2.e4ed7087.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/michael-scott-memoryGame/static/js/2.e4ed7087.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fcfb2974d119dca198",
+    "url": "/michael-scott-memoryGame/static/js/main.5ba5455c.chunk.js"
+  },
+  {
+    "revision": "6d9b74eb4c38317c537f",
+    "url": "/michael-scott-memoryGame/static/js/runtime-main.299b8956.js"
+  }
+]);
